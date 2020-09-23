dfdfc332e73d59cd0b6194823a3099a92c256dd7
Henrique Marcelino <henrique.marcelino1996@gmail.com>
2020-09-23 16:00:08 -0300

    Fazendo seleções de dados de duas tabelas

---
+++ Aulas iniciais/Henrique_Aula_23-09_20_DML.sql
@@ -0,0 +1,7 @@
+use loja_veiculos;
+
+select * from proprietarios;
+
+select * from tb_veiculo;
+
+select * from tb_veiculo INNER JOIN proprietarios ON tb_veiculo.proprietarios_id_dono = proprietarios.id_dono;
